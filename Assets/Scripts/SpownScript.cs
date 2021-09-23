bdca10d04c91bbe86d431a7215b05392041f3426
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-09-23 22:19:04 +0530

    comit

---
+++ Assets/Scripts/SpownScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpownScript : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawing());
+    }
+
+    IEnumerator StartSpawing()
+    {
+        yield return new WaitForSeconds(4);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawing());
+    }
+}
